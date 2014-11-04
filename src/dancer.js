72307dc89ca5c5c35598a3613791972ad61deb54
Hack Reactor Students <accounts@hackreactor.com>
2014-11-03 19:55:26 -0800

    Added space background

--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,5 @@
 var Dancer = function(top, left, timeBetweenSteps){
-  this.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');//optional dancer
   this.timeBetweenSteps = timeBetweenSteps;
 
 
